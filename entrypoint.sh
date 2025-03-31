0d653c31aba565071aa3cdaea992710a39b4b143
DarksoulHP19 <harshjpatel1974@gmail.com>
2025-03-31 11:02:26 +0530

    ec2 deployment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# Replace placeholder with the runtime environment variable
-envsubst '${VITE_API_BASE_URL}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
+# Replace placeholder with runtime environment variable
+echo "Injecting environment variables..."
+envsubst '${VITE_API_BASE_URL}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp    # <-- Environment injection
 mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
 
 # Start NGINX
